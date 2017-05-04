bf0ed0513afcfb4e59cd04d736110403dbbd6afc
olivier lamy <olamy@apache.org>
2017-05-04 10:22:36 +1000

    get rid of spdy option
    Signed-off-by: olivier lamy <olamy@apache.org>

--- src/java/winstone/HttpsConnectorFactory.java
+++ src/java/winstone/HttpsConnectorFactory.java
@@ -142,23 +142,7 @@ public class HttpsConnectorFactory implements ConnectorFactory {
 
     private ServerConnector createConnector(Server server, Map args) {
         SslContextFactory sslcf = getSSLContext(args);
-        if (Option.HTTPS_SPDY.get(args)) {// based on http://wiki.eclipse.org/Jetty/Feature/SPDY
-            try {
-                sslcf.setIncludeProtocols("TLSv1");
-                return (ServerConnector)Class.forName("org.eclipse.jetty.spdy.server.http.HTTPSPDYServerConnector")
-                        .getConstructor(Server.class, SslContextFactory.class)
-                        .newInstance(server,sslcf);
-            } catch (NoClassDefFoundError e) {
-                if (e.getMessage().contains("org/eclipse/jetty/npn")) {
-                    // a typically error is to forget to run NPN
-                    throw new WinstoneException(SSL_RESOURCES.getString("HttpsListener.MissingNPN"), e);
-                }
-                throw e;
-            } catch (Exception e) {
-                throw new Error("Failed to enable SPDY connector",e);
-            }
-        } else
-            return new ServerConnector(server,sslcf);
+        return new ServerConnector(server,sslcf);
     }
 
     private static PrivateKey readPEMRSAPrivateKey(Reader reader) throws IOException, GeneralSecurityException {
