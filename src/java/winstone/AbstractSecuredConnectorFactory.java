8082e6f5e95bca19a2b4b98ad4f1ed4df95319f4
olivier lamy <olamy@apache.org>
2017-07-11 18:35:48 +1000

    restore original license
    Signed-off-by: olivier lamy <olamy@apache.org>

--- src/java/winstone/AbstractSecuredConnectorFactory.java
+++ src/java/winstone/AbstractSecuredConnectorFactory.java
@@ -1,21 +1,8 @@
 /*
- * Copyright Olivier Lamy
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Copyright 2003-2006 Rick Knowles <winstone-devel at lists sourceforge net>
+ * Distributed under the terms of either:
+ * - the common development and distribution license (CDDL), v1.0; or
+ * - the GNU Lesser General Public License, v2.1 or later
  */
 
 package winstone;
