bcd18111b73cc0c6d3542bde2c9667ce80da2bb6
Christian.Rohr <Ch.Rohr@gmx.de>
2019-12-09 22:44:58 +0100

    Fixed SonarQube issues and create XML Entity class

---
+++ src/main/java/winstone/entities/User.java
@@ -0,0 +1,47 @@
+package winstone.entities;
+
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement(name = "user")
+public class User {
+
+    @XmlElement(name = "username")
+    private String username;
+
+    @XmlElement(name = "password")
+    private String password;
+
+    @XmlElement(name = "rolename")
+    private String roleList;
+
+    public User(String username, String password, String roleList) {
+        this.username = username;
+        this.password = password;
+        this.roleList = roleList;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRoleList() {
+        return roleList;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRoleList(String roleList) {
+        this.roleList = roleList;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
