9cb70be0bb2a77afa68c040d6a5a87ae969618be
Christian.Rohr <Ch.Rohr@gmx.de>
2020-02-22 20:37:37 +0100

    Corrected import command

--- src/main/java/winstone/ServerConnectorBuilder.java
+++ src/main/java/winstone/ServerConnectorBuilder.java
@@ -1,6 +1,10 @@
 package winstone;
 
-import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.server.ForwardedRequestCustomizer;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 public class ServerConnectorBuilder {
