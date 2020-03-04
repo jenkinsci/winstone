076201f964f0c23af0071805a151628f79f81ef9
Jesse Glick <jglick@cloudbees.com>
2020-03-04 15:13:12 -0500

    Reproduced problem in unit test

---
+++ src/test/java/winstone/testApplication/servlets/AcceptFormServlet.java
@@ -0,0 +1,26 @@
+package winstone.testApplication.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AcceptFormServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        try (PrintWriter pw = resp.getWriter()) {
+            try {
+                req.getParameterNames();
+            } catch (Exception x) {
+                x.printStackTrace(pw);
+                return;
+            }
+            pw.print("received " + req.getContentLength() + " bytes");
+        }
+    }
+
+}
