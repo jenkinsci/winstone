8e95faefcbfe5ca232489c06cc13098b9d7f56c0
Basil Crow <me@basilcrow.com>
2022-02-07 13:34:58 -0800

    Delete unused import

--- src/main/java/winstone/HostConfiguration.java
+++ src/main/java/winstone/HostConfiguration.java
@@ -25,7 +25,6 @@ import java.io.OutputStream;
 import java.lang.reflect.Constructor;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
-import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Hashtable;
