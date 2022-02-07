d4d0ed1aa4b5f2aae9945c7d6817c57bdb667371
Basil Crow <me@basilcrow.com>
2022-02-07 15:10:04 -0800

    Fix high-threshold SpotBugs warnings

--- src/main/java/winstone/WinstoneSession.java
+++ src/main/java/winstone/WinstoneSession.java
@@ -1,5 +1,7 @@
 package winstone;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 /**
  * Expose the property to remain compatible with Winstone 1.0
  *
@@ -9,7 +11,7 @@ public class WinstoneSession {
     /**
      * Name of the cookie that stores HTTP session ID.
      */
-    // @SuppressFBWarnings(value = "MS_SHOULD_BE_FINAL", justification = "mutated by extras-executable-war")
+    @SuppressFBWarnings(value = "MS_SHOULD_BE_FINAL", justification = "mutated by extras-executable-war")
     public static String SESSION_COOKIE_NAME = "JSESSIONID";
 
     private WinstoneSession() {}
