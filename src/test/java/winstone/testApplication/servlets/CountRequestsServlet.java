876d6bc5fbc099168d2325f4986c80e4e35453b1
Basil Crow <me@basilcrow.com>
2024-08-27 13:11:09 -0700

    [JENKINS-73259] Migrate Winstone from EE 8 to EE 9

--- src/test/java/winstone/testApplication/servlets/CountRequestsServlet.java
+++ src/test/java/winstone/testApplication/servlets/CountRequestsServlet.java
@@ -6,12 +6,12 @@
  */
 package winstone.testApplication.servlets;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * Simple test servlet that counts the number of times it has been requested,
